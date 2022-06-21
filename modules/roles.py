65827ebdef8d6f5908f40710ba9ca93c04d589e3
pandarey <bartei12.mic@gmail.com>
2022-06-21 23:11:36 +0200

    Weryfikacja wieku konta (anty-raid)

--- modules/roles.py
+++ modules/roles.py
@@ -1,28 +1,31 @@
 import discord
 import config
+import datetime as dt
+from pytz import UTC
 
 
 async def add(bot, payload, guild):
-    if payload.member == bot.user:
-        return
-    if payload.channel_id == 963763374133493812:
-        await payload.member.add_roles(discord.utils.get(guild.roles, id=config.role[payload.emoji.name]))
+    if UTC.localize(dt.datetime.utcnow() + dt.timedelta(days=-7)) > payload.member.created_at:
+        if payload.member == bot.user:
+            return
+        if payload.channel_id == 875029163034181633:
+            await payload.member.add_roles(discord.utils.get(guild.roles, id=config.role[payload.emoji.name]))
 
-    elif payload.channel_id == 963763374133493810:
-        if payload.emoji.name != '✅':
-            user = guild.get_member(payload.user_id)
-            if discord.utils.get(user.roles, id=963763373001048138):
+        elif payload.channel_id == 875008016309706773:
+            if payload.emoji.name != '✅':
+                user = guild.get_member(payload.user_id)
+                if discord.utils.get(user.roles, id=875053632217813022):
+                    await payload.member.add_roles(discord.utils.get(guild.roles, id=config.zone[payload.emoji.name]))
+            else:
                 await payload.member.add_roles(discord.utils.get(guild.roles, id=config.zone[payload.emoji.name]))
-        else:
-            await payload.member.add_roles(discord.utils.get(guild.roles, id=config.zone[payload.emoji.name]))
 
 
 async def remove(payload, guild):
-    if payload.channel_id == 963763374133493812:
+    if payload.channel_id == 875029163034181633:
         user = guild.get_member(payload.user_id)
         await user.remove_roles(discord.utils.get(guild.roles, id=config.role[payload.emoji.name]))
-    elif payload.channel_id == 963763374133493810:
+    elif payload.channel_id == 875008016309706773:
         if payload.emoji.name != '✅':
             user = guild.get_member(payload.user_id)
-            if discord.utils.get(user.roles, id=963763373001048138):
+            if discord.utils.get(user.roles, id=875053632217813022):
                 await user.remove_roles(discord.utils.get(guild.roles, id=config.zone[payload.emoji.name]))
